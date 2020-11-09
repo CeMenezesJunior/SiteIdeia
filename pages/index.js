dbfaa44dcd124c68afe4acadae85d2f8a85cc4db
Carlos Eduardo Menezes Junior <carloseduardojunior@id.uff.br>
2020-11-09 19:40:31 -0300

    Pagina atualizada contador

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
+import { useState } from "react";
+function CapsLock(props){
+    const textoInserido = props.children;
+    const textoEmCapsLock = textoInserido.toUpperCase();
+    return <div>{textoEmCapsLock}</div>
+}
+
+function Contador(){
+    const [contador, setContador] = useState(1)
+    function adicionarContador(){
+        setContador(contador + 1);
+        console.log("Adicionou");
+    }
+    return(
+        <div>
+            <div>{contador}</div>
+            <button onClick={adicionarContador}>Adiciona</button>
+        </div>
+    )
+}
 
 function Home(){
     return (
         <div>
-            <h1>Home</h1>
-            <p>Jogo</p>
-            <div>Teste</div>
+            <Contador/>
+            <CapsLock>Seja feliz</CapsLock>
         </div>
     )
 }
