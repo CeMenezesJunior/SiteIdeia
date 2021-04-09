3af850f2dbaf9ed85a6f61ea388ec7e1c1312625
Carlos Eduardo Menezes Junior <carloseduardojunior@id.uff.br>
2021-04-09 01:57:09 -0300

    Novos ajustes

--- pages/blog.js
+++ pages/blog.js
@@ -5,7 +5,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function getStaticProps(){
-    const posts = await prisma.Post.findMany();
+    const posts = await prisma.post.findMany();
     for (var i=0;i<posts.length;i++){
         posts[i].createAt = JSON.parse(JSON.stringify(posts[i].createAt))
     }
