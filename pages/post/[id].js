d2ce50e9ff9116785786f0252e551dcbea3f9b2e
Carlos Eduardo Menezes Junior <carloseduardojunior@id.uff.br>
2021-04-20 02:06:46 -0300

    Ajuste de tela para static

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,12 +1,20 @@
-import { faPooStorm } from '@fortawesome/free-solid-svg-icons';
+import React from 'react'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient;
 
-export async function getServerSideProps({ params }) {
+export async function getStaticPaths() {
+    return{
+        paths:[{params:{id:"1"}},{params:{id:"2"}},{params:{id:"3"}}],
+        fallback:false
+    }
+
+}
+
+export async function getStaticProps(context) {
     const postCerto = await prisma.post.findFirst({
         where: {
-            id: Number(params.id)
+            id: Number(context.params.id)
         }
     });
     postCerto.createAt = JSON.parse(JSON.stringify(postCerto.createAt))
